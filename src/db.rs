cb3d142a419b64081ff5003ce715958b9cdb664d
Vadim Palatov <vadim.palatav@yandex.ru>
2024-07-27 01:58:13 +0300

    Constants deleted, literals moved to actual calls.
    As long as we have only one function wich uses this coostants
    and also because of one of this function calls only once
    at start - constants nor unnmed and
    left only as literal in function code.
    let compiler optimize this better then me.

--- src/db.rs
+++ src/db.rs
@@ -6,22 +6,7 @@ use std::io::{Read, Write, Seek, SeekFrom};
 use rusqlite::Connection;
 //NOTE: ORM  в данном случаи не применяю.
 
-const SQL_CREATE_TABLE_QUERY : &str = 
-"CREATE TABLE IF NOT EXISTS todos ( 
-    Name VARCHAR(32) PRIMARY KEY, /* Спорно, можно было  саоздать ID 
-    но судя по ТЗ name считается уникальным само по себе. */
-    Description TEXT NOT NULL, 
-    Category VARCHAR(32) NOT NULL, 
-    Is_complteed BOOLEAN DEFAULT FALSE REFERENCES status, 
-    Creation_date_time INTEGER NOT NULL 
-    
-)
-"; //NOTE: Этот литерал, вроде как, в ходе работы программы не содержит переносов.
-const SQL_INIT_TABLE_INDEX_QUERY: &str =  "CREATE INDEX catg ON todos (Cateory)" ;
-
-const SQL_INSERT_QUERY_TEMPLATE: &str = "send "; 
-const SQL_UPDATE_QUERY_TEMPLATE: &str = "it's fine.";
-
+ 
 /// Обёртка, существует для более удобного 
 /// вызова функций, связанных с БД и 
 /// для консистенции кода. 
@@ -34,9 +19,18 @@ impl ConnHandler {
 pub fn load_db(file_path: &Path) -> Self {
     // Файл базы пригодный? 
     let conn  = Connection::open(file_path).unwrap();
-    conn.execute(SQL_CREATE_TABLE_QUERY, ());
+    conn.execute("CREATE TABLE IF NOT EXISTS todos ( 
+    Name VARCHAR(32) PRIMARY KEY, /* Спорно, можно было бы  саоздать ID 
+    но судя по ТЗ name считается уникальным само по себе. */
+    Description TEXT NOT NULL, 
+    Category VARCHAR(32) NOT NULL, 
+    Is_complteed BOOLEAN DEFAULT FALSE REFERENCES status, 
+    Creation_date_time INTEGER NOT NULL 
     
-    conn.execute(SQL_INIT_TABLE_INDEX_QUERY, ()); 
+)
+", ());
+    
+    conn.execute("CREATE INDEX catg ON todos (Cateory)", ()); 
     // Если мы смлшди лькоыьб файд как БД. а в ходе проверки индекса 
     // (с влмзможным его созданием) = то мы можес спокойно отдать соединение.
     return ConnHandler { conn  }
